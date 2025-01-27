1584b97011e3d13238146df7c7006da95b400107
subedi1680 <subedi1680@gmail.com>
2025-01-27 13:25:38 +0545

    Color-Picker

---
+++ Color Picker/src/App.jsx
@@ -0,0 +1,6 @@
+import ColorPicker from "./colorPicker.jsx";
+
+function App(){
+  return(<ColorPicker />);
+}
+export default App;
---
+++ Color Picker/src/colorPicker.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+function ColorPicker(){
+
+    const [color,setColor] = useState("#FFFFFF");
+
+    function handleColorChange(event){
+        setColor(event.target.value);
+    }
+
+    return(<div className="color-picker-container">
+
+        <h1>COLOR PICKER</h1>
+        <div className="color-display" style={{backgroundColor: color}}>
+            <p>Selected Color : {color}</p>
+        </div>
+        <label>Select a color:</label>
+        <input type="color" value={color} onChange={handleColorChange} />
+    </div>);
+}
+export default ColorPicker;
